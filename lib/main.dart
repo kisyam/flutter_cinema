c34ccc3608c41a4812b557339addad6ff4b9f371
kisyam <how2logout@gmail.com>
2023-05-17 20:23:52 +0900

    comingsoon listview builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:cinemas/screens/home_screen.dart';
-import 'package:cinemas/services/api_service.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +11,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Cinemas API',
       home: HomeScreen(),
     );
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,38 +2,65 @@ import 'package:cinemas/models/movie_model.dart';
 import 'package:cinemas/services/api_service.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
+  //* future type이 있기 때문에 const 가 올 수 없다.
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<MovieModel> movies = [];
-  bool isLoading = true;
-  // final Future<List<MovieModel>> comingSoonMovies = ApiService.getComingSoon();
-
-  void waitForMovies() async {
-    movies = await ApiService.getComingSoon();
-    isLoading = false;
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    waitForMovies();
-  }
+  final Future<List<MovieModel>> movies = ApiService.getComingSoon();
+  static const basePosterUrl = 'https://image.tmdb.org/t/p/w500';
 
   @override
   Widget build(BuildContext context) {
-    print(movies);
-    print(isLoading);
     return Scaffold(
-      body: Center(
-        child: Text('${movies.length}'),
-      ),
-    );
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 20,
+                ),
+                child: Text(
+                  'Coming soon',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+              ),
+              FutureBuilder(
+                future: movies,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      children: [
+                        SizedBox(
+                          height: 310.0,
+                          child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: snapshot.data!.length,
+                            itemBuilder: (context, index) {
+                              var movie = snapshot.data![index];
+                              return Image.network(
+                                  "$basePosterUrl${movie.posterPath}");
+                            },
+                          ),
+                        ),
+                      ],
+                    );
+                  }
+                  return const Center(
+                    child: CircularProgressIndicator.adaptive(),
+                  );
+                },
+              )
+            ],
+          ),
+        ));
   }
 }
